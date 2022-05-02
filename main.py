d16d3f659e3ebb9af82bf7a9580ee7b7da37839d
ramshafirdous <104720378+ramshafirdous@users.noreply.github.com>
2022-05-02 19:09:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-#Step 5
-
 import random
 
-#TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
-#Delete this line: word_list = ["ardvark", "baboon", "camel"]
+#Update the word list to use the 'word_list' from hangman_words.py
 from hangman_words import word_list
 
 chosen_word = random.choice(word_list)
@@ -12,12 +9,9 @@ word_length = len(chosen_word)
 end_of_game = False
 lives = 6
 
-#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+#Import the logo from hangman_art.py and print it at the start of the game.
 from hangman_art import logo
 print(logo)
-
-#Testing code
-print(f'Pssst, the solution is {chosen_word}.')
 
 #Create blanks
 display = []
@@ -27,7 +21,7 @@ for _ in range(word_length):
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
-    #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+    #TODO-4:If the user has entered a letter they've already guessed, print the letter and let them know.
     if guess in display:
         print(f"You've already guessed {guess}")
 
@@ -40,7 +34,7 @@ while not end_of_game:
 
     #Check if user is wrong.
     if guess not in chosen_word:
-        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+        #TODO-5:If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
         print(f"You guessed {guess}, that's not in the word. You lose a life.")
         
         lives -= 1
@@ -56,6 +50,6 @@ while not end_of_game:
         end_of_game = True
         print("You win.")
 
-    #TODO-2: - Import the stages from hangman_art.py and make this error go away.
+    #TODO-2: Import the stages from hangman_art.py and make this error go away.
     from hangman_art import stages
     print(stages[lives])
